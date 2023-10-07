c2e0dadd13100e3162499ec92a00e9bd5cc88e88
kytho <kythonlk@gmail.com>
2023-10-07 23:05:03 +0400

    update simple home

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Photography Studio';
+export const SITE_DESCRIPTION = 'We are passionate and creative photographers';
